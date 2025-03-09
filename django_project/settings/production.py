871300f451f7ecf6672ebf43096771d88ebc1c2f
David Naldo <152567331+Naldz777@users.noreply.github.com>
2025-03-10 01:57:26 +0800

    Update production.py

--- django_project/settings/production.py
+++ django_project/settings/production.py
@@ -1,19 +1,40 @@
 from .base import *
+import environ
 
-DEBUG = False
+env = environ.Env()
+environ.Env.read_env(BASE_DIR / ".env")  # Ensure your .env file is read
 
-ALLOWED_HOSTS = ['127.0.0.1']
 
-# Production database settings
-DATABASES = { #hosted somewhere else
+
+DEBUG = True #determines if local (dev) mode.
+
+
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'OPTIONS': {
-            "read_default_file": ".venv/my.cnf"  
-        }
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': env('DB_NAME', default='fallback_db_name'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env.int('DB_PORT', default=3306),  # Convert to integer
     }
 }
 
-# Static files (production)
-STATIC_ROOT = '/var/www/your_static_files/'
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587 #gmail port
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+ADMIN_EMAIL = env('ADMIN_EMAIL')
+
+# Local static and media file settings
+STATICFILES_DIRS = [BASE_DIR / 'static']
+from django.apps import AppConfig
+
+
+class PagesConfig(AppConfig):
+    name = "pages"
